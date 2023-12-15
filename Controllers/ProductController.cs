9cceeede5b305129369866ba08058bbef0e4eb3d
Alex Fang <106351391+alexfang324@users.noreply.github.com>
2023-12-15 09:54:45 -0800

    added ViewModel folder

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SSD_Major_Web_Project.Models;
+
+namespace SSD_Major_Web_Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly NovaDbContext _context;
+
+        public ProductController(NovaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModels/ProductTileViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace SSD_Major_Web_Project.Models.ViewModels.Shared
+{
+    public class ProductTileViewModel
+    {
+    }
+}
