3bd3c778cbc5a63c4092c39455d6e81302719aec
Charlotte Chak <cchak4@my.bcit.ca>
2024-02-14 11:35:00 -0800

    footer css

--- SSD-Major-Web-Project/Program.cs
+++ SSD-Major-Web-Project/Program.cs
@@ -4,7 +4,7 @@ using SSD_Major_Web_Project.Data;
 using SSD_Major_Web_Project.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+//var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
 
 // Add services to the container.
