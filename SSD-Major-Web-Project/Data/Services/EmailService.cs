110679b97a9890a73c64d2c7f1b41fdabdb9d796
Amanda Mar <msamandamar@gmail.com>
2024-02-23 11:47:43 -0800

    Email confirmation and captcha setup, with debugging

---
+++ SSD-Major-Web-Project/Data/Services/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using SSD_Major_Web_Project.Models;
+using System.Net.Mail;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace SSD_Major_Web_Project.Data.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<Response> SendSingleEmail(ComposeEmailModel payload)
+        {
+            var apiKey = _configuration.GetSection("SendGrid")["ApiKey"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("msamandamar@gmail.com", "Amanda Mar");
+            var subject = payload.Subject;
+            var to = new EmailAddress(payload.Email
+                                     , $"{payload.FirstName} {payload.LastName}");
+            var textContent = payload.Body;
+            var htmlContent = $"<strong>{payload.Body}</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject
+                                                    , textContent, htmlContent);
+            var request = client.SendEmailAsync(msg);
+            request.Wait();
+            var result = request.Result;
+            return request;
+        }
+    }
+
+}
---
+++ SSD-Major-Web-Project/Data/Services/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using SendGrid;
+using SSD_Major_Web_Project.Models;
+
+namespace SSD_Major_Web_Project.Data.Services
+{
+    public interface IEmailService
+    {
+        Task<Response> SendSingleEmail(ComposeEmailModel payload);
+    }
+
+}
