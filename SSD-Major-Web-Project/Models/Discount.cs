cca71b7396f4c7f3756725325a73f0b437e4c640
Alex Fang <106351391+alexfang324@users.noreply.github.com>
2024-02-26 15:32:54 -0800

    update

--- SSD-Major-Web-Project/Models/Discount.cs
+++ SSD-Major-Web-Project/Models/Discount.cs
@@ -1,10 +1,21 @@
-﻿namespace SSD_Major_Web_Project.Models;
+﻿using System;
+using System.Collections.Generic;
+
+namespace SSD_Major_Web_Project.Models;
 
 public partial class Discount
 {
     public string PkDiscountCode { get; set; } = null!;
 
     public double DiscountValue { get; set; }
+
+    public string DiscountType { get; set; } = null!;
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly EndDate { get; set; }
+
+    public string IsActive { get; set; } = null!;
 
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 }
