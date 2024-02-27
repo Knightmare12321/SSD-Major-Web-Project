62d95bf6982552f490b6ed93e08f383e534b896c
Charlotte Chak <cchak4@my.bcit.ca>
2024-02-27 13:36:57 -0800

    first fix

--- SSD-Major-Web-Project/Controllers/ShopController.cs
+++ SSD-Major-Web-Project/Controllers/ShopController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using SSD_Major_Web_Project.Models;
-using SSD_Major_Web_Project.Repositories;   
+using SSD_Major_Web_Project.Repositories;
 using SSD_Major_Web_Project.ViewModels;
 using System;
+using System.Net;
 
 namespace SSD_Major_Web_Project.Controllers
 {
@@ -21,45 +23,146 @@ namespace SSD_Major_Web_Project.Controllers
 
         public IActionResult Index()
         {
-            List<Product> products = _context.Products.ToList();
-            //for each product in product, assign ImageByteArray to product.Image
+            try
+            {
+                // catch error, if no products found, show error message " There is no product available now, please come back later"
+                if (_context.Products.Count() == 0)
+                {
+                    return View("Error", new ErrorViewModel { RequestId = "Your shopping cart is empty" });
+                }
+                else
+                {
 
+                    //List<Product> products = _context.Products.ToList();
 
+                    // assign products to shoppingcartVM using a product array harcoded
+                    List<Product> products = new List<Product>
+                    {
+                        new Product { PkProductId = 1, Name = "Product 1", Price = 100 },
+                        new Product { PkProductId = 2, Name = "Product 2", Price = 200 },
+                        new Product { PkProductId = 3, Name = "Product 3", Price = 300 }
+                    };
+
+                    ShoppingCartVM shoppingcartVM = new ShoppingCartVM();
+                    //shoppingcart.UserId = "user123";
+                    ShopRepo _shopRepo = new ShopRepo(_context);
+                    shoppingcartVM.Subtotal = _shopRepo.CalculateSubtotal(products);
+                    shoppingcartVM.ShippingFee = 0;
+                    shoppingcartVM.Taxes = _shopRepo.CalculateTaxes(shoppingcartVM.Subtotal);
+                    shoppingcartVM.GrandTotal = _shopRepo.CalculateGrandTotal(shoppingcartVM.Subtotal, shoppingcartVM.Taxes, shoppingcartVM.ShippingFee);
+
+                    shoppingcartVM.Products = products;
+
+                    return View(shoppingcartVM);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in ShopController");
+                return View("Error", new ErrorViewModel { RequestId = "Your shopping cart is empty" });
+            }
+        }
+
+        // GET: ShopController/Checkout
+        [HttpPost]
+        public IActionResult Checkout(CheckoutVM vm)
+        {
+            //List<Product> products = _context.Products.ToList();
+            List<Product> products = new List<Product>
+            {
+                new Product { PkProductId = 1, Name = "Product 1", Price = 100 },
+                new Product { PkProductId = 2, Name = "Product 2", Price = 200 },
+                new Product { PkProductId = 3, Name = "Product 3", Price = 300 }
+            };
 
             ShoppingCartVM shoppingcartVM = new ShoppingCartVM();
 
-            //shoppingcart.UserId = "user123";
-            ShopRepository _shopRepo = new ShopRepository(_context);
+            ShopRepo _shopRepo = new ShopRepo(_context);
             shoppingcartVM.Subtotal = _shopRepo.CalculateSubtotal(products);
             shoppingcartVM.ShippingFee = 0;
-            shoppingcartVM.Taxes = _shopRepo.CalculateTaxes(shoppingcartVM.Subtotal) ;
+            shoppingcartVM.Taxes = _shopRepo.CalculateTaxes(shoppingcartVM.Subtotal);
             shoppingcartVM.GrandTotal = _shopRepo.CalculateGrandTotal(shoppingcartVM.Subtotal, shoppingcartVM.Taxes, shoppingcartVM.ShippingFee);
-
+            shoppingcartVM.Currency = "CAD";
+            shoppingcartVM.CurrencySymbol = "$";
             shoppingcartVM.Products = products;
 
-            return View(shoppingcartVM);
+            //pass the shoppingcart data to Checkout view model
+            vm.ShoppingCart = shoppingcartVM;
+
+            // if user is not logged in, ask user to enter address
+            //shoppingcartVM.Order.Contact.FirstName = FirstName;
+            //shoppingcartVM.Order.Contact.LastName = LastName;
+            //shoppingcartVM.Order.Contact.Address = address;
+            //shoppingcartVM.Order.Contact.Address2 = address2;
+            //shoppingcartVM.Order.Contact.City = city;
+            //shoppingcartVM.Order.Contact.province = province;
+            //shoppingcartVM.Order.Contact.Country = country;
+            //shoppingcartVM.Order.Contact.PostalCode = postalCode;
+            //shoppingcartVM.Order.Contact.Phone = phone;
+
+            // if user logged in, display user's default adress as shipping option
+            return View("Checkout", vm);
         }
 
-        // GET: HomeController1/Create
-        public ActionResult Create()
+
+        public IActionResult OrderConfirmation(OrderConfirmationVM orderConfirmationModel)
         {
-            return View();
+            return View(orderConfirmationModel);
         }
 
-        // POST: HomeController1/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+
+
+
+
+        //// GET: ShopController/CreateOrder
+        //public IActionResult CreateOrder()
+        //{
+        //    return View();
+        //}
+
+        // POST: ShopController/CreateOrder
+        //[HttpPost]
+        //public async Task<IActionResult> CreateOrder(Order order)
+        //{
+        //    // check if data valid
+        //    if (ModelState.IsValid)
+        //    {
+        //        // create order
+        //        order = new Order();
+        //        order.OrderDate = DateOnly;
+        //        order.FkAddress = vm.ShippingAddress;
+        //        order.ShippingCity = vm.ShippingCity;
+        //        order.ShippingCountry = vm.ShippingCountry;
+        //        order.ShippingPostalCode = vm.ShippingPostalCode;
+        //        order.ShippingState = vm.ShippingState;
+        //        order.UserId = "user123";
+        //        order.OrderItems = new List<OrderItem>();
+
+        //        // create order items
+        //        foreach (Product product in vm.Products)
+        //        {
+        //            OrderItem orderItem = new OrderItem();
+        //            orderItem.ProductId = product.Id;
+        //            orderItem.Quantity = product.Quantity;
+        //            orderItem.Price = product.Price;
+        //            order.OrderItems.Add(orderItem);
+        //        }
+
+        //        // save order to database
+        //        _context.Orders.Add(order);
+        //        await _context.SaveChangesAsync();
+
+        //        // redirect to order confirmation page
+        //        return RedirectToAction("OrderConfirmation", new { orderId = order.Id });
+        //    }
+        //    else
+        //    {
+        //        return View(vm);
+        //    }
+
+
+        //}
+
 
     }
 }
---
+++ SSD-Major-Web-Project/ViewModels/CheckcoutVM.cs
@@ -0,0 +1,11 @@
+﻿namespace SSD_Major_Web_Project.ViewModels
+{
+    public class CheckoutVM
+    {
+        public OrderVM Order { get; set; }
+        public ShoppingCartVM ShoppingCart { get; set; }
+
+        public string currency { get; set; }
+
+    }
+}
