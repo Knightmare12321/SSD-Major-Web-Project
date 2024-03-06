0005de168eb2fa683afc0be0b4508a34e328e968
Amanda Mar <msamandamar@gmail.com>
2024-03-06 15:08:23 -0800

    authorization debugging

--- SSD-Major-Web-Project/ViewModels/RoleVM.cs
+++ SSD-Major-Web-Project/ViewModels/RoleVM.cs
@@ -5,9 +5,6 @@ namespace SSD_Major_Web_Project.ViewModels
     public class RoleVM
     {
         [Required]
-        public string? Id { get; set; }
-
-        [Required]
         [Display(Name = "Role Name")]
         public string? RoleName { get; set; }
 
